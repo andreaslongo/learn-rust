f46f2cd57e40577d25ee5abec8142bc3cdc16b98
Andreas Longo <git@andreaslongo.com>
2023-07-09 14:09:18 +0200

    Add parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -13,6 +13,27 @@ fn functions() {
     assert_eq!(result, "a string value");
 }
 
+#[test]
+fn parameters() {
+    // Parameters are special variables that are part of a function's signature.
+    fn function_with_parameter(x: i32) -> String {
+        format!("You passed {x} as an argument to my parameter.")
+    }
+
+    // When calling a function, we can provide concrete values for it's parameters to input data.
+    // The values passed in when you call a function are called arguments.
+    let result = function_with_parameter(5);
+    assert_eq!(result, "You passed 5 as an argument to my parameter.");
+
+    // Defining multiple parameters
+    fn function_with_two_parameters(value: i32, unit_label: char) -> String {
+        format!("The measurement is: {value}{unit_label}")
+    }
+
+    let result = function_with_two_parameters(5, 'h');
+    assert_eq!(result, "The measurement is: 5h");
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // Helper functions
 ///////////////////////////////////////////////////////////////////////////////
