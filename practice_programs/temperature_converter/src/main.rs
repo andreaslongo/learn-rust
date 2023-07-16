7952ad26032b756510cf072232e20e9be75cddde
Andreas Longo <git@andreaslongo.com>
2023-07-16 14:45:37 +0200

    Add celsius to fahrenheit

--- practice_programs/temperature_converter/src/main.rs
+++ practice_programs/temperature_converter/src/main.rs
@@ -1,8 +1,12 @@
 // Converts temperatures between Fahrenheit and Celsius.
 fn main() {
-    let f = 60.0;
+    let f = 68.0;
     let c = to_celsius(f);
     println!("{f} degrees Fahrenheit is equal to {c} degrees Celsius.");
+
+    let c = 20.0;
+    let f = to_fahrenheit(c);
+    println!("{c} degrees Celsius is equal to {f} degrees Fahrenheit.");
 }
 
 #[test]
@@ -17,6 +21,21 @@ fn convert_fahrenheit_to_celsius() {
 }
 
 fn to_celsius(fahrenheit: f32) -> f32 {
-    // Single precision should be enough for displaying a human readable temperature.
+    // Single precision should be enough for a human readable temperature.
     (fahrenheit - 32.0) * 5.0 / 9.0
+}
+
+#[test]
+fn convert_celsius_to_fahrenheit() {
+    let celsius = 20.0;
+    let fahrenheit = to_fahrenheit(celsius);
+    assert_eq!(fahrenheit, 68.0);
+
+    let celsius = 15.0;
+    let fahrenheit = to_fahrenheit(celsius);
+    assert_eq!(fahrenheit, 59.0);
+}
+
+fn to_fahrenheit(celsius: f32) -> f32 {
+    celsius * 9.0 / 5.0 + 32.0
 }
