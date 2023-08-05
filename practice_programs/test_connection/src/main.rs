0abf89b6cc27dcd0e565a3ddaa2fdff379864089
Andreas Longo <git@andreaslongo.com>
2023-08-05 23:01:58 +0200

    Set port based on protocol name for convenience

--- practice_programs/test_connection/src/main.rs
+++ practice_programs/test_connection/src/main.rs
@@ -3,28 +3,46 @@ use std::net::ToSocketAddrs;
 use std::time::Duration;
 
 use clap::Parser;
+use clap::ValueEnum;
 
-/// Simple program to test TCP connectivity
+/// A simple program to test TCP connectivity
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// DNS name or IP address
     host: String,
 
-    /// TCP port
-    port: u32,
+    /// TCP port number
+    #[arg(value_parser = clap::value_parser!(u16).range(1..))]
+    port: Option<u16>,
+
+    /// Protocol
+    #[arg(short, long, conflicts_with("port"))]
+    protocol: Option<Protocol>,
 
     /// Wait limit for each connection in seconds
     #[arg(short, long, default_value_t = 1)]
-    timeout: u64,
+    timeout: u16,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Protocol {
+    Http,
+    Https,
+    Smb,
 }
 
 fn main() {
     let args = Args::parse();
+    let timeout_in_seconds = Duration::new(args.timeout.into(), 0);
+    let port: u16 = match args.protocol {
+        Some(Protocol::Http) => 80,
+        Some(Protocol::Https) => 443,
+        Some(Protocol::Smb) => 445,
+        None => args.port.unwrap_or(443),
+    };
 
-    let timeout_in_seconds = Duration::new(args.timeout, 0);
-    let connection = format!("{}:{}", args.host, args.port);
-
+    let connection = format!("{}:{}", args.host, port);
     let addrs_iter = connection.to_socket_addrs().unwrap();
     for addr in addrs_iter {
         match TcpStream::connect_timeout(&addr, timeout_in_seconds) {
