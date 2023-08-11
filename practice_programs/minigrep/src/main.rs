052bf626be3159eb4dcae652e3938d3edc0474a3
Andreas Longo <git@andreaslongo.com>
2023-08-11 17:46:53 +0200

    Improve error handling

--- practice_programs/minigrep/src/main.rs
+++ practice_programs/minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for '{}'", config.query);
     println!("In file '{}'", config.file_path);
@@ -21,11 +25,14 @@ struct Config<'a> {
 }
 
 impl Config<'_> {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = &args[1];
         let file_path = &args[2];
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }
 
