41ae8fce8ce875dc46d22e9cfb4ff3b7f3a2b075
Andreas Longo <git@andreaslongo.com>
2023-09-25 22:10:53 +0200

    Return HTML

--- practice_programs/web_server/src/main.rs
+++ practice_programs/web_server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -23,4 +24,11 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
