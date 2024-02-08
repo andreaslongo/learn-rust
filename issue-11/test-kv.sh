fab4ce9723fa528b80f06f60c45e0078756f54ae
Andreas Longo <git@andreaslongo.com>
2024-02-08 21:18:18 +0100

    Test kv store with image data

--- issue-11/test-kv.sh
+++ issue-11/test-kv.sh
@@ -11,12 +11,23 @@ curl \
     --data 'test content for key-2'
 
 curl \
+    --request POST \
+    --url http://127.0.0.1:3000/kv/image-1 \
+    --header 'Content-Type: image/png' \
+    --data-binary @test.png
+
+curl \
     --request GET \
     --url http://127.0.0.1:3000/kv/key-1 \
 
 curl \
     --request GET \
     --url http://127.0.0.1:3000/kv/key-2 \
+
+curl \
+    --request GET \
+    --url http://127.0.0.1:3000/kv/image-1 \
+    > ./test2.png
 
 curl \
     --request GET \
--- issue-11/test-process-image.sh
+++
@@ -1,1 +0,0 @@
-curl --header "Content-Type: image/png" --data-binary @test.png http://127.0.0.1:3000/process > test2.png
