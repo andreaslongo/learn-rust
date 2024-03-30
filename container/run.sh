5699895060bac79d0cf19671f3ecbcf48cf80d54
Andreas Longo <git@andreaslongo.com>
2024-03-30 21:12:35 +0100

    Apply changes from template

--- container/run.sh
+++ container/run.sh
@@ -8,15 +8,14 @@ readonly local uid=1000
 readonly local gid=1000
 
 # Use --publish <host_port>:<container_port> to enable networking.
-podman container attach "$(basename ${parent_dir})" 2>/dev/null || podman container run \
+# Use --rm to remove the container after exit.
+podman container start --attach --interactive "$(basename ${parent_dir})" 2>/dev/null || podman container run \
     --interactive \
     --name="$(basename ${parent_dir})" \
-    --publish 8001:7878 \
     --pull=newer \
-    --rm \
     --tty \
     --user ${uid}:${gid} \
     --userns keep-id:uid=${uid},gid=${gid} \
-    --volume "${parent_dir}":/home/appuser/app:Z,rw \
-    --workdir /home/appuser/app \
+    --volume "${parent_dir}:/home/appuser/$(basename ${parent_dir}):Z,rw" \
+    --workdir "/home/appuser/$(basename ${parent_dir})" \
         localhost/"$(basename ${parent_dir})":latest
